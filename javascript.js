46c726d437fe11b273455ab954542ad5f712f053
tomyberlinger <49924622+tomyberlinger@users.noreply.github.com>
2025-09-30 12:15:05 -0300

    ahora con js separado

---
+++ javascript.js
@@ -0,0 +1,12 @@
+<script>
+    // Your JavaScript goes here!
+    let name = "John";
+    let surname = "Doe";
+    alert("✅ El script se está ejecutando");
+    console.log(name);
+    console.log(surname);
+    const string = "The revolution will not be televised.";
+    console.log(string); 
+
+
+</script>
