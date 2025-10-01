faa32f28be67041394835cbd4f21e35f8b09ff64
tomyberlinger <49924622+tomyberlinger@users.noreply.github.com>
2025-10-01 20:06:09 -0300

    esto sube nuevo JS para probar functions

--- javascript.js
+++ javascript.js
@@ -42,3 +42,28 @@ function setWeather() {
     para.textContent = "";
   }
 }
+
+function add7 (num) {
+  return num + 7;
+}
+console.log(add7(3));
+
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+console.log(multiply(3,4));
+
+function capitalize (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+console.log(capitalize("hello"));
+console.log(capitalize("HELLO"));
+console.log(capitalize("hElLo"));
+
+function lastLetter (string) {
+  return string.charAt(string.length - 1);
+}
+console.log(lastLetter("hello"));
+console.log(lastLetter("goodbye"));
+console.log(lastLetter("Zebra"));
+console.log(lastLetter("banana"));  
